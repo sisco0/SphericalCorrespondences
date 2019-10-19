70fe93c3d91d3cc906c3d45d8fa8c0cf3a242856
sisco_0 <siscomagma@gmail.com>
2019-10-18 21:16:16 -0500

    Need rework, added asift
    Added Asift

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import cv2
 import cv2.xfeatures2d
 import numpy as np
 import itertools
+from asift import affine_detect, init_feature, affine_detect, filter_matches
+from multiprocessing.pool import ThreadPool
+from common import Timer
 
 class Image:
     def __init__(self, filename):
@@ -18,17 +21,11 @@ if __name__ == "__main__":
         Image('res/pano2tenpercent.jpg'),
         Image('res/pano3tenpercent.jpg')
     ]
-    #Once images are loaded calc SIFT of each one
-    sift1 = cv2.xfeatures2d.SIFT_create(
-        nfeatures=2000,
-        nOctaveLayers=3,
-        contrastThreshold=0.05,
-        edgeThreshold=6,
-        sigma=1.6
-    )
-    mask = np.ones(shape=np.shape(images[0].data)[0:2],dtype=np.uint8)
+    #Once images are loaded calc ASIFT of each one
+    detector, matcher = init_feature('brisk-flann')
+    pool=ThreadPool(processes = cv2.getNumberOfCPUs())
     for image in images:
-        image.kp, image.kpdes = sift1.detectAndCompute(image.data, mask)
+        image.kp, image.kpdes = affine_detect(detector, image.data, pool=pool)
         imageMarkers = image.data.copy()
         for marker in image.kp:
             imageMarkers = cv2.drawMarker(
@@ -39,15 +36,19 @@ if __name__ == "__main__":
         # cv2.imshow('Image with markers: ' + image.filename,imageMarkers)
         # cv2.waitKey(0)
     # Once all features are obtained then match images for each pair
-    FLANN_INDEX_KDTREE = 0
-    matcher = cv2.FlannBasedMatcher(
-        dict(algorithm=FLANN_INDEX_KDTREE, trees=5),
-        dict(checks=50))
     n = 0
     for pair in itertools.combinations(images,2):
-        matches = matcher.match(
-            pair[0].kpdes,
-            pair[1].kpdes)
+        with Timer('matching'):
+            raw_matches = matcher.knnMatch(pair[0].kpdes, trainDescriptors = pair[1].kpdes, k = 2)
+        p1, p2, kp_pairs = filter_matches(pair[0].kp, pair[1].kp, raw_matches)
+        if len(p1) >= 4:
+            H, status = cv2.findHomography(p1, p2, cv.RANSAC, 5.0)
+            print('%d / %d  inliers/matched' % (np.sum(status), len(status)))
+            # do not draw outliers (there will be a lot of them)
+            kp_pairs = [kpp for kpp, flag in zip(kp_pairs, status) if flag]
+        else:
+            H, status = None, None
+            print('%d matches found, not enough for homography estimation' % len(p1))
         matches = sorted(matches, key = lambda x: x.distance)
         imMatches = cv2.drawMatches(
             pair[0].data, pair[0].kp,
